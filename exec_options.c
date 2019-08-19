67207d9ab77588ef3d6e4f4a03515b341270f091
Arata Yano <aratayano00@gmail.com>
2019-08-19 17:43:08 +0200

    added a exec_l_flag function that will handle the -l flag specifically

--- exec_options.c
+++ exec_options.c
@@ -1,21 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ls.c                                            :+:      :+:    :+:   */
+/*   exec_options.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ayano <ayano@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/19 14:27:42 by ayano             #+#    #+#             */
-/*   Updated: 2019/08/19 17:05:51 by ayano            ###   ########.fr       */
+/*   Created: 2019/08/19 16:54:35 by ayano             #+#    #+#             */
+/*   Updated: 2019/08/19 17:13:39 by ayano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int		main(int argc, char **argv)
+void	exec_options(t_options **options)
 {
-	t_options	*options;
+	t_files		*files;
 
-	options = init_options(argc, argv);
-	exex_options(&options);
+	files = init_files();
+	if ((*options)->l == 1)
+		exec_l_flag(&files);
+	// TODO -a -t -r -R.
+	return ;
+}
+
+t_files	*init_files(void)
+{
+	t_files		*files;
+
+	files = (t_files *)malloc(sizeof(t_files));
+	files->head = NULL;
+	files->name = NULL;
+	files->next = NULL;
+	files->time = 0;
+	return(files);	
 }
--- ft_ls.h
+++ ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: ayano <ayano@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/19 14:05:51 by ayano             #+#    #+#             */
-/*   Updated: 2019/08/19 16:52:06 by ayano            ###   ########.fr       */
+/*   Updated: 2019/08/19 17:13:09 by ayano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,15 @@ typedef struct			s_options
 	int					R;					
 }						t_options;
 
+typedef struct			s_files
+{
+	char				*name;
+	int					time;
+	struct s_files		*head;
+	struct s_files		*next;
+}						t_files;
+
+
 /*
 ** functions to set options, check errors and
 ** initialize the struct.
@@ -42,5 +51,16 @@ t_options				*init_options(int argc, char **argv);
 void					set_options(t_options **options, int argc, char **argv);
 void					check_input(int argc, char **argv);
 
+/*
+** function that determines which function to call
+** and then prints the resultant.
+*/
+void					exec_options(t_options **options);
+t_files					*init_files(void);
+
+/*
+** functions for the -l flag.
+*/
+t_files					*exec_l_flag(t_files **files);
 
 #endif
